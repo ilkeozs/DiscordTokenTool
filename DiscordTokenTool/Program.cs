23c085b88dddde96ae1079ec26bf632b49820ea1
İlkeOzs <ilkeozsekerli@gmail.com>
2024-06-07 15:09:16 +0300

    Add project files.

---
+++ DiscordTokenTool/Program.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordTokenTool
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            string title = @"
+______ _                       _   _____     _                _____           _ 
+|  _  (_)                     | | |_   _|   | |              |_   _|         | |
+| | | |_ ___  ___ ___  _ __ __| |   | | ___ | | _____ _ __     | | ___   ___ | |
+| | | | / __|/ __/ _ \| '__/ _` |   | |/ _ \| |/ / _ \ '_ \    | |/ _ \ / _ \| |
+| |/ /| \__ \ (_| (_) | | | (_| |   | | (_) |   <  __/ | | |   | | (_) | (_) | |
+|___/ |_|___/\___\___/|_|  \__,_|   \_/\___/|_|\_\___|_| |_|   \_/\___/ \___/|_|
+                                                                                ";
+            Console.WriteLine(title);
+            Console.ResetColor();
+
+            Console.Write("\nEmail or Phone Number: ");
+            string email = Console.ReadLine();
+            Console.Write("Password: ");
+            string password = Console.ReadLine();
+            await getToken(email, password);
+        }
+
+        async static Task getToken(string email, string password)
+        {
+            string url = "https://discord.com/api/v9/auth/login";
+
+            var payload = new Dictionary<string, string>()
+            {
+                { "login", email}, { "password", password }
+            };
+
+            string jsonPayload = JsonConvert.SerializeObject(payload);
+            HttpClient client = new HttpClient();
+            StringContent stringContent = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await client.PostAsync(url, stringContent);
+
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("\nLogin Successful!");
+                string responseContent = await response.Content.ReadAsStringAsync();
+                JObject json = JObject.Parse(responseContent);
+                Console.WriteLine($"User ID: {json["user_id"]}");
+                Console.Write($"Token: {json["ticket"]}");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("\nLogin Failed!");
+                Console.Write($"Error: {response.ReasonPhrase}");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ DiscordTokenTool/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DiscordTokenTool")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DiscordTokenTool")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4d3cd54d-3501-4547-9c2e-ddbc95688cd7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
